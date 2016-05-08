a1fb6f188791080422fb4204f88eaaf7048633d6
peterpanwl <wangliang0222@gmail.com>
2016-05-08 12:00:10 +0800

    git改用pull的方式，因为push要出错

--- push.pl
+++ push.pl
@@ -1,48 +1,35 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-# 把本目录master分支推送到directories中的其他目录的master分支
 
-my $order;
-if (@ARGV == 1) {
-    ($order) = @ARGV;
-    if ($order eq '-f') {
-        die "wrong option, only -f or --force\n";
-    }
-}elsif (@ARGV > 1) {
-    die "only support one option\n";
-}
+@ARGV == 1 or die "Usage: perl $0 branch name\n";
 
-#获取本目录地址以便跳过
-my ($pwd) = `pwd`;
-chomp($pwd);
-my (@address) = split /\//, $pwd;
-my $jump;
-foreach (@address) {
-    $jump = $_;
-};
-# 打开日志文件
-open(DIR, '< ../directories') or die "Could not open file 'directories' $!";
-open(LOG, '>> ../push.txt') or die "Could not open file 'push.txt' $!";
+my ($branch) = @ARGV;
+my $remote = `pwd`;
+chomp($remote);
+
+chdir '../';
+open(DIR, '< ./directories') or die "Could not open file 'directories' $!";
+open(LOG, '>> ./push.txt') or die "Could not open file 'push.txt' $!";
+
 # 运行时间写入日志文件
 my ($date) = `date`;
 print LOG $date;
 
 foreach (<DIR>) {
     chomp;
-    if ($jump eq $_) {
-        print LOG "$_ 目录跳过\n";
-        next;
-    }
-    my $remote = "../$_";
-    if (-e "$remote/info") {
-        system "git remote add temprepo $remote";
-        if (defined($order)) {
-            system "git push $order $remote master:master";
+    if (-e "$_/info") {
+        chdir $_;
+        my $jump = `pwd`;
+        chomp($jump);
+        if ($remote ne $jump) {
+            system "git remote add temprepo $remote";
+            system "git pull $remote $branch";
+            system "git remote remove temprepo";
         }else{
-            system "git push $remote master:master";
+            print LOG "$jump 目录是源目录，跳过\n";
         }
-        system "git remote remove temprepo";
+        chdir '../';
     }else{
         print LOG "$_ 没有这个目录\n";
     }
