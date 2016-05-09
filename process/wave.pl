6421d53a39c28a24f0ecf335b3b19f8fabc6939a
peterpanwl <wangliang0222@gmail.com>
2016-05-09 22:25:10 +0800

    改变波形

---
+++ process/wave.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+#打开日志文件
+open(LOG, '>> ../process.md') or die "Could not open file 'process.md' $!";
+print LOG "\n\n## $0";
+
+open(INFO, '< ../info') or die "Could not open file 'info' $!";
+my @sacorder;
+my $i = 1;
+foreach (<INFO>) {
+    chomp;
+    my ($a, $b) = split ':';
+    if ($a eq 'WAVE') {
+        push @sacorder, $b;
+        $i = 0;
+    }
+}
+
+if ($i == 0) {
+    open(SAC, "| sac") or die "Error in opening SAC\n";
+    print SAC "wild echo off\n";
+    print SAC "r *.SAC\n";
+    foreach (@sacorder) {
+        print SAC "$_\n";
+    }
+    print SAC "w over\n";
+    print SAC "q\n";
+    close(SAC);
+}
+
+print LOG "\n$0正常结束"; 
+close(LOG);
